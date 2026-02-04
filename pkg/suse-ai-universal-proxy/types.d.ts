9587e6d85e9cf92b3dc0b5e6f3c6a17e7ad16c11
alessandro-festa <175830926+alessandro-festa@users.noreply.github.com>
2026-02-04 10:20:07 +0100

    renaming

---
+++ pkg/suse-ai-universal-proxy/types.d.ts
@@ -0,0 +1,9 @@
+declare module '@shell/*' {
+  const shellComponent: any
+  export default shellComponent
+}
+
+declare module '@components/*' {
+  const componentsComponent: any
+  export default componentsComponent
+}
